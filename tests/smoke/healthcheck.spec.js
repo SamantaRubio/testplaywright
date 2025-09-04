a6f5a54839a8151ec130e649d7489188fd7ef02e
SamantaRubio <samanta.rubio@magmalabs.io>
2025-09-04 12:04:01 -0600

    new slack message 1

--- tests/smoke/healthcheck.spec.js
+++ tests/smoke/healthcheck.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('Healthcheck', () => {
   test('App renders and shows main container', async ({ page }) => {
     await page.goto('/');
-    await expect(page.getByTestId('new4535rfff')).toBeVisible();
+    await expect(page.getByTestId('new_user')).toBeVisible();
   });
 });
 
