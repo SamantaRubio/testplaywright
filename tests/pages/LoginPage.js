c910d99adde79aaabe0d0453b0e75acdf5aebffb
SamantaRubio <samanta.rubio@magmalabs.io>
2025-09-09 13:14:25 -0600

    nuevo slack msj

--- tests/pages/LoginPage.js
+++ tests/pages/LoginPage.js
@@ -1,8 +1,6 @@
 import { BasePage } from './BasePage.js';
 import { expect } from '@playwright/test';
 
-//Cambio para PR
-
 export class LoginPage extends BasePage {
   //Constructor: define all the page elements
   constructor(page) {
